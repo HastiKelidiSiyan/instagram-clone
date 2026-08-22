2342de52266739a6cfa06a0cd0c98479991249e9
Hasti Kelidi Siyan <hastikelidisiyan@gmail.com>
2026-08-22 18:52:36 +0330

    Refactored remote data source's error handling.

--- lib/data_source/remote_data_source.dart
+++ lib/data_source/remote_data_source.dart
@@ -1,4 +1,5 @@
 import 'package:dio/dio.dart';
+import 'package:instagram_clone/models/app_failure.dart';
 import 'package:instagram_clone/models/message_model.dart';
 import 'package:instagram_clone/models/post_model.dart';
 import 'package:instagram_clone/models/story_model.dart';
@@ -32,117 +33,130 @@ class RemoteDataSource {
 
   Future<List<PostModel>> getPosts() async {
     try {
-      final response = await dio.get('$postsAndMessagesBaseUrl/posts');
+      final response = await _dio.get('$postsAndMessagesBaseUrl/posts');
       if (response.statusCode == 200) {
-        return (response.data as List)
-            .map((json) => PostModel.fromJson(json))
-            .toList();
+        final List<dynamic> data = response.data;
+        return data.map((json) => PostModel.fromJson(json)).toList();
       } else {
         throw Exception('Failed to load posts');
       }
-    } catch (e) {
-      throw Exception('Failed to load posts');
+    } on DioException catch (e) {
+      throw mapDioException(e);
     }
   }
 
   Future<List<MessageModel>> getMessages() async {
     try {
-      final response = await dio.get('$postsAndMessagesBaseUrl/messages');
+      final response = await _dio.get('$postsAndMessagesBaseUrl/messages');
       if (response.statusCode == 200) {
-        return (response.data as List)
-            .map((json) => MessageModel.fromJson(json))
-            .toList();
+        final List<dynamic> data = response.data;
+        return data.map((json) => MessageModel.fromJson(json)).toList();
       } else {
-        throw Exception('Failed to load messages');
+        throw Exception('${response.statusCode}: Failed to load messages');
       }
-    } catch (e) {
-      throw Exception('Failed to load messages');
+    } on DioException catch (e) {
+      throw mapDioException(e);
     }
   }
 
   Future<List<StoryModel>> getStories() async {
     try {
-      final response = await dio.get('$usersAndStoriesBaseUrl/stories');
+      final response = await _dio.get('$usersAndStoriesBaseUrl/stories');
       if (response.statusCode == 200) {
-        return (response.data as List)
-            .map((json) => StoryModel.fromJson(json))
-            .toList();
+        final List<dynamic> data = response.data;
+        return data.map((json) => StoryModel.fromJson(json)).toList();
       } else {
-        throw Exception('Failed to load stories');
+        throw Exception('${response.statusCode}: Failed to load stories');
       }
-    } catch (e) {
-      throw Exception('Failed to load stories');
+    } on DioException catch (e) {
+      throw mapDioException(e);
     }
   }
 
   Future<List<UserModel>> getUsers() async {
     try {
-      final response = await dio.get('$usersAndStoriesBaseUrl/users');
+      final response = await _dio.get('$usersAndStoriesBaseUrl/users');
       if (response.statusCode == 200) {
-        return (response.data as List)
-            .map((json) => UserModel.fromJson(json))
-            .toList();
+        final List<dynamic> data = response.data;
+        return data.map((json) => UserModel.fromJson(json)).toList();
       } else {
-        throw Exception('Failed to load users');
+        throw Exception('${response.statusCode}: Failed to load users');
       }
-    } catch (e) {
-      throw Exception('Failed to load users');
+    } on DioException catch (e) {
+      throw mapDioException(e);
     }
   }
 
   Future<UserModel?> getUserById(int id) async {
     try {
-      final response = await dio.get(
+      final response = await _dio.get(
         '$usersAndStoriesBaseUrl/users',
         queryParameters: {'userId': id},
       );
       if (response.statusCode == 200) {
-        final users = response.data as List;
-        if (users.isEmpty) {
-          return null;
-        }
-        return UserModel.fromJson(users.first as Map<String, dynamic>);
+        return UserModel.fromJson(response.data[0]);
       } else {
-        throw Exception('Failed to load user');
+        throw Exception('${response.statusCode}: Failed to load user');
       }
-    } catch (e) {
-      throw Exception('Failed to load user');
+    } on DioException catch (e) {
+      throw mapDioException(e);
     }
   }
 
   Future<UserModel?> getUserByUsername(String username) async {
     try {
-      final response = await dio.get(
+      final response = await _dio.get(
         '$usersAndStoriesBaseUrl/users',
         queryParameters: {'username': username},
       );
       if (response.statusCode == 200) {
-        final users = response.data as List;
-        if (users.isEmpty) {
-          return null;
-        }
-        return UserModel.fromJson(users.first as Map<String, dynamic>);
+        return UserModel.fromJson(response.data[0]);
       } else {
-        throw Exception('Failed to load user');
+        throw Exception('${response.statusCode}: Failed to load user');
       }
-    } catch (e) {
-      throw Exception('Failed to load user');
+    } on DioException catch (e) {
+      throw mapDioException(e);
     }
   }
 
   Future<UserModel?> addUser(UserModel user) async {
     try {
-      final response = await dio.post(
+      final response = await _dio.post(
         '$usersAndStoriesBaseUrl/users',
         data: user.toJson(),
       );
       if (response.statusCode == 201) {
         return UserModel.fromJson(response.data);
       } else {
-        throw Exception('Failed to load user');
+        throw Exception('${response.statusCode}: Failed to add user');
       }
-    } catch (e) {
-      throw Exception('Failed to load user');
+    } on DioException catch (e) {
+      throw mapDioException(e);
     }
   }
+
+  AppFailure mapDioException(DioException exception) {
+  return switch (exception.type) {
+    DioExceptionType.badCertificate =>
+      const BadCertificateFailure(),
+
+    DioExceptionType.badResponse =>
+      const ServerFailure(),
+
+    DioExceptionType.cancel =>
+      const RequestCancelledFailure(),
+
+    DioExceptionType.connectionError =>
+      const NetworkFailure(),
+
+    DioExceptionType.connectionTimeout ||
+    DioExceptionType.receiveTimeout ||
+    DioExceptionType.sendTimeout ||
+    DioExceptionType.transformTimeout =>
+      const TimeoutFailure(),
+
+    DioExceptionType.unknown =>
+      const UnknownFailure(),
+  };
+}
 }
