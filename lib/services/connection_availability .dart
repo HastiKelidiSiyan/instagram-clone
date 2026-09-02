98e791b538e87ffe355004d9d42263632f0d34ba
Hasti Kelidi Siyan <hastikelidi@gmail.com>
2026-09-02 13:25:35 +0330

    Refactored connection availability check.

--- lib/services/connection_availability .dart
+++ lib/services/connection_availability .dart
@@ -3,9 +3,10 @@ import 'package:connectivity_plus/connectivity_plus.dart';
 class ConnectionAvailibility {
   Future<bool> isConnected() async {
     final connectivityResult = await Connectivity().checkConnectivity();
-    if (connectivityResult.contains(ConnectivityResult.none)) {
-      return false;
+    if (connectivityResult.contains(ConnectivityResult.mobile) ||
+        connectivityResult.contains(ConnectivityResult.wifi)) {
+      return true;
     }
-    return true;
+    return false;
   }
 }
